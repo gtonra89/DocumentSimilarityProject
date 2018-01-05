6ae25009afef794236e68125457b3f593d6cd416
gtonra89 <gtonra89@gmail.com>
2018-01-05 15:08:50 +0000

    added document class and storing documents to Db4o database

---
+++ src/ie/gmit/sw/Document.java
@@ -0,0 +1,47 @@
+package ie.gmit.sw;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Document {
+	private String DocumentTitle;
+	private ArrayList<String> list = new ArrayList<String>();
+	
+	public Document(String documentTitle, ArrayList<String> list) {
+		super();
+		DocumentTitle = documentTitle;
+		this.list = list;
+	}
+
+	public Document() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDocumentTitle() {
+		return DocumentTitle;
+	}
+
+	public void setDocumentTitle(String documentTitle) {
+		DocumentTitle = documentTitle;
+	}
+
+	public ArrayList<String> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<String> list) {
+		this.list = list;
+		for(int i = 0 ; i < list.size(); i++){
+			System.out.println("blahha :" + list.get(i).toString());
+		}
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+}
